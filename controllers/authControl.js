abb8f0b5d9d982b39940220c314e17964ccd9299
Ravi Shankar <67696330+ravis-web@users.noreply.github.com>
2020-07-25 02:06:39 +0530

    promises <> async-await

--- controllers/authControl.js
+++ controllers/authControl.js
@@ -8,38 +8,33 @@ const User = require('../models/User');
 const errOccured = require('../utils/errors').errOccured;
 const errHandler = require('../utils/errors').errHandler;
 
-exports.regUser = (req, res, next) => {
+exports.regUser = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) errOccured('validation failed!', 422);
-  bcrypt.hash(req.body.password, 12) // hashing
-    .then(hashed => {
-      const user = new User({ password: hashed, name: req.body.name, email: req.body.email });
-      return user.save();
-    })
-    .then(user => res.status(201).json({ msg: 'user-created', userId: user._id }))
-    .catch(err => errHandler(err, next));
+  try {
+    const hashed = await bcrypt.hash(req.body.password, 12); // hashing
+    const user = new User({ password: hashed, name: req.body.name, email: req.body.email });
+    await user.save();
+    res.status(201).json({ msg: 'user-created', userId: user._id });
+  } catch (err) { errHandler(err, next); }
 };
 
-exports.loginUser = (req, res, next) => {
+exports.loginUser = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) errOccured('validation failed', 422);
-  let currUser;
 
-  // password-match
-  User.findOne({ email: req.body.email })
-    .then(user => {
-      currUser = user; // fx-scoped
-      return bcrypt.compare(req.body.password, user.password);
-    })
-    .then(doMatch => {
-      if (!doMatch) errOccured('username/password incorrect', 401);
+  try {
+    // password-match
+    const user = await User.findOne({ email: req.body.email });
+    const doMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!doMatch) errOccured('username/password incorrect', 401);
 
-      // create token
-      const token = jsonwt.sign(
-        { userId: currUser._id.toString(), email: currUser.email }, // JSON data
-        'long-string', // secret-key
-        { expiresIn: '1h' }); // expiry
-      res.status(200).json({ token: token, userId: currUser._id.toString() });
-    })
-    .catch(err => errHandler(err, next));
+    // create token
+    const token = jsonwt.sign(
+      { userId: user._id.toString(), email: user.email }, // JSON data
+      'long-string', // secret-key
+      { expiresIn: '1h' }); // expiry
+    res.status(200).json({ token: token, userId: user._id.toString() });
+
+  } catch (err) { errHandler(err, next); }
 };
--- controllers/feedControl.js
+++ controllers/feedControl.js
@@ -9,23 +9,18 @@ const errHandler = require('../utils/errors').errHandler;
 const Post = require('../models/Post');
 const User = require('../models/User');
 
-exports.fetchPosts = (req, res, next) => {
+exports.fetchPosts = async (req, res, next) => {
   const curPage = req.query.page || 1;
   const perPage = 2; // sync w frontend
-  let totalPost = 0;
-  Post.find().countDocuments()
-    .then(count => {
-      totalPost = count;
-      return Post.find().skip((curPage - 1) * perPage).limit(perPage);
-    })
-    .then(posts => {
-      if (!posts) errOccured('no posts found', 404);
-      res.status(200).json({ posts: posts, totalItems: totalPost });
-    })
-    .catch(err => errHandler(err, next));
+  try {
+    const totalPost = await Post.find().countDocuments();
+    const posts = await Post.find().populate('creator').skip((curPage - 1) * perPage).limit(perPage);
+    if (!posts) errOccured('no posts found', 404);
+    res.status(200).json({ posts: posts, totalItems: totalPost });
+  } catch (err) { errHandler(err, next); }
 };
 
-exports.createPost = (req, res, next) => {
+exports.createPost = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) errOccured('validation failed!', 422);
 
@@ -39,7 +34,6 @@ exports.createPost = (req, res, next) => {
 
   if (!req.file) errOccured('no file selected', 422);
 
-  let creator;
   const post = new Post({
     title: req.body.title,
     content: req.body.content,
@@ -47,39 +41,33 @@ exports.createPost = (req, res, next) => {
     // image: req.file.path, // Linux and OSX
     creator: req.userId
   });
-  post.save()
-    .then(reslt => {
-      return User.findById(req.userId)
-    })
-    .then(user => {
-      creator = user;
-      user.posts.push(post); // mongoose adds ref. by default
-      return user.save();
-    })
-    .then(reslt => {
-      res.status(201).json({
-        message: 'post created!',
-        post: post,
-        creator: { _id: creator._id, name: creator.name }
-      });
-    })
-    .catch(err => errHandler(err, next));
+
+  try {
+    await post.save();
+    const user = await User.findById(req.userId);
+    user.posts.push(post); // mongoose adds ref. by default
+    await user.save();
+    res.status(201).json({
+      message: 'post created!',
+      post: post,
+      creator: { _id: user._id, name: user.name }
+    });
+  } catch (err) { errHandler(err, next); }
   /* --- async-code ---
   if (!err.statusCode) err.statusCode = 500;
   next(err);
   */
 };
 
-exports.fetchPost = (req, res, next) => {
-  Post.findById(req.params.id)
-    .then(post => {
-      if (!post) errOccured('no matching post', 404);
-      res.status(200).json({ post: post });
-    })
-    .catch(err => errHandler(err, next));
+exports.fetchPost = async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params.id).populate('creator');
+    if (!post) errOccured('no matching post', 404);
+    res.status(200).json({ post: post });
+  } catch (err) { errHandler(err, next); }
 };
 
-exports.updatePost = (req, res, next) => {
+exports.updatePost = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) errOccured('validation failed!', 422);
   let image = req.body.image;
@@ -88,56 +76,54 @@ exports.updatePost = (req, res, next) => {
     // image = req.file.path.; // Linux and OSX
   }
   if (!image) errOccured('no image found', 422);
-  Post.findById(req.params.id)
-    .then(post => {
-      if (!post) errOccured('no matching post', 404);
-      if (post.creator.toString() !== req.userId) errOccured('post created by another user', 403);
-      post.title = req.body.title;
-      post.content = req.body.content;
-      if (image !== post.image) {
-        deleteFile(post.image);
-        post.image = image;
-      }
-      return post.save();
-    })
-    .then(reslt => res.status(200).json({ post: reslt }))
-    .catch(err => errHandler(err, next));
-};
 
-exports.deletePost = (req, res, next) => {
-  Post.findById(req.params.id)
-    .then(post => {
-      if (!post) errOccured('no matching post', 404);
-      if (post.creator.toString() !== req.userId) errOccured('post created by another user', 403);
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) errOccured('no matching post', 404);
+    if (post.creator.toString() !== req.userId) errOccured('post created by another user', 403);
+    post.title = req.body.title;
+    post.content = req.body.content;
+    if (image !== post.image) {
       deleteFile(post.image);
-      return Post.findByIdAndRemove(req.params.id);
-    })
-    .then(reslt => User.findById(req.userId))
-    .then(user => {
-      user.posts.pull(req.params.id); // mongoose removes ref from user
-      return user.save();
-    })
-    .then(reslt => res.status(200).json({ msg: 'post deleted!' }))
-    .catch(err => errHandler(err, next));
+      post.image = image;
+    }
+    await post.save();
+    res.status(200).json({ post: post });
+  } catch (err) { errHandler(err, next); }
 };
 
-exports.fetchStatus = (req, res, next) => {
-  User.findById(req.userId).then(user => {
+exports.deletePost = async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) errOccured('no matching post', 404);
+    if (post.creator.toString() !== req.userId) errOccured('post created by another user', 403);
+    deleteFile(post.image);
+    await Post.findByIdAndRemove(req.params.id);
+    const user = await User.findById(req.userId);
+    user.posts.pull(req.params.id); // mongoose removes ref from user
+    await user.save();
+    res.status(200).json({ msg: 'post deleted!' });
+  } catch (err) { errHandler(err, next); }
+};
+
+exports.fetchStatus = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.userId);
     if (!user) errOccured('user not found', 404);
     res.status(200).json({ status: user.status });
-  }).catch(err => errHandler(err, next));
+  } catch (err) { errHandler(err, next); }
 };
 
-exports.updateStatus = (req, res, next) => {
+exports.updateStatus = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) errOccured('validation failed!', 422);
-
-  User.findById(req.userId).then(user => {
+  try {
+    const user = await User.findById(req.userId);
     if (!user) errOccured('user not found', 404);
     user.status = req.body.status;
-    return user.save();
-  }).then(user => res.status(200).json({ msg: 'status-updated', status: user.status }))
-    .catch(err => errHandler(err, next));
+    await user.save();
+    res.status(200).json({ msg: 'status-updated', status: user.status });
+  } catch (err) { errHandler(err, next); }
 };
 
 const deleteFile = filepath => {
